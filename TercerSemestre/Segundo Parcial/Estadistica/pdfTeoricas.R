315ee53bf8e3e99313765fd08fc36cccbc1a3ef9
Jefferson David Yepez Moran <jeffersonyepez621@gmail.com>
2024-07-15 10:09:10 -0500

    Prueba estadistica

---
+++ TercerSemestre/Segundo Parcial/Estadistica/pdfTeoricas.R
@@ -0,0 +1,25 @@
+#En un laboratorio de computacion con 30 computadores se encontraron 7 con virus
+#b. El Intervalo de confianza correspondiente es (8%, 38%)
+
+
+#Las varianzas de tiempos de producción en 100 corridas realizadas en dos máquinas similares son 112 y 91 minutos cuadrados
+#respectivamente
+#c. Uno de los estadísticos F es igual a 1.48
+
+
+#De 70 pedidos realizados al proveedor A, 10 llegaron con retraso. Además, 6 de 100 pedidos del proveedor B llegaron atrasados
+#b. El porcentaje de pedidos retrasados entre ambos proveedores se pueden considerar iguales
+
+
+#Se desea comparar los tiempos de producción entre dos maquinas similares. Para esto, se analizan 100 corridas de producción en cada
+#máquina obteniendo promedios de 17.5 y 20.7 min, y desviaciones muestrales de 1.2 y 2.2 min. Suponga poblaciones normales con varianzas
+#iguales
+#c. Los tiempos medios entre ambas máquinas son distintos
+
+
+#50 ventas en un almacen arrojaron una media de 150 dolares con una desviacion de 33 usd.
+#c. El error muestral correspondiente es igual a 9.37
+
+#6.- En una muestra de 10 computadores se encontro un promedio de 5 viruscon una varianza de 0.25
+#a. El estadistico T es igual a 2.26
+
